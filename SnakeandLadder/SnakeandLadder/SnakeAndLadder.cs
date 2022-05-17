d9a333a3aec5213b0a3b2d636194052b9bbf4c11
Reshma2310 <dreshma253@gmail.com>
2022-05-17 19:27:06 +0500

    [Reshma] - Initial Position of Player

---
+++ SnakeandLadder/SnakeandLadder/SnakeAndLadder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeandLadder
+{
+    internal class SnakeAndLadder
+    {
+        int startPosition = 0, playerPosition = 0;
+        public void Position()
+        { 
+        Console.WriteLine("Initial position of player is " + playerPosition);
+        }
+    }
+}
