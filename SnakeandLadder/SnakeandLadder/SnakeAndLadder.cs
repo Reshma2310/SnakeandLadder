00e45ff4e1cd5edf9e2db2b87743f7c4d03141f7
Reshma2310 <dreshma253@gmail.com>
2022-05-17 21:15:43 +0500

    [Reshma] - Option of either noPlay, Snake or Ladder

--- SnakeandLadder/SnakeandLadder/SnakeAndLadder.cs
+++ SnakeandLadder/SnakeandLadder/SnakeAndLadder.cs
@@ -8,12 +8,27 @@ namespace SnakeandLadder
 {
     internal class SnakeAndLadder
     {
-        int startPosition = 0, playerPosition = 0;
+        public int startPosition = 0, playerPosition = 0;
+        const int noPlay = 0, snake = 1, ladder = 2;
         Random random = new Random();
         public void diceRoll()
         {
-            int diceNo = random.Next(1, 6);
-        Console.WriteLine("Dice value of player is " + diceNo);
+        int diceNo = random.Next(1, 6);
+            switch (random.Next(0, 3))
+            {
+                case noPlay:
+                    playerPosition += 0;
+                    break;
+                case snake:
+                    playerPosition -= diceNo;
+                    break;
+                case ladder:
+                    playerPosition += diceNo;
+                    break;
+                default:
+                    break;
+            }
+        Console.WriteLine("Dice value of player is {0} with Position at {1}", diceNo,playerPosition);
         }
     }
 }
